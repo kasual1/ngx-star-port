335785b0e215b113440eb0501d4809cdfd16bfd2
Lukas Seifert <seifertluk11@gmail.com>
2022-05-16 21:20:04 +0200

    Fix assets path in sample data

--- projects/demo/src/app/data/starships.data.ts
+++ projects/demo/src/app/data/starships.data.ts
@@ -4,55 +4,55 @@ import { StarShip } from "../models/star-ship.model";
         {
             id: 1,
             name: 'Alderaan',
-            imageUrl: '../assets/alderaan.png',
+            imageUrl: 'assets/alderaan.png',
             description: `Alderan was the first planet to be shown on-screen as a victim of the power of the Death Star, which blew the entire space rock up in an instant. This meant that all of the planet’s inhabitants lost their lives in a matter of seconds before they even knew that they were going to get targeted by the Death Star as a way for Moff Tarkin to force Leia into revealing the location of the base where the Rebels primarily operated from.`
         },
         {
             id: 2,
             name: 'Coruscant',
-            imageUrl: '../assets/coruscant.png',
+            imageUrl: 'assets/coruscant.png',
             description: `Coruscant was, for millennia, the capital of the Republic and the place where the Jedi Temple used to stand. It also became the capital of the Empire during Emperor Palpatine’s reign. That is how important this planet is, as it played a pivotal role in the prequel movies. And if you look at the Star Wars Legends continuity, Coruscant also played an important role in the wars between the Jedi and the Sith.`
         },
         {
             id: 3,
             name: 'Geonosis',
-            imageUrl: '../assets/geonosis.png',
+            imageUrl: 'assets/geonosis.png',
             description: `Geonosis, the planet of the Geonosians, who were responsible for building the Separatist droid army, plays a very important role in the events of the prequel trilogy movies and during the time of the Clone War. Of course, Geonosis was also the planet where one of the biggest events in Star Wars history happened.`
         },
         {
             id: 4,
             name: 'Hoth',
-            imageUrl: '../assets/hoth.png',
+            imageUrl: 'assets/hoth.png',
             description: `Hoth became the base of operations for the Rebellion right after they had to abandon Yavin 4. This was not the best planet to be on because of its freezing temperatures. Of course, the Empire also pushed the Rebels to a wall when they were on Hoth. Nevertheless, we cannot downplay how important Hoth is because the Empire was able to make the Rebels realize that, after their victory on Yavin 4, they still had their work cut out for them.`
         },
         {
             id: 5,
             name: 'Jakku',
-            imageUrl: '../assets/jakku.png',
+            imageUrl: 'assets/jakku.png',
             description: `There is something between Star Wars and Lucasfilm’s love for desert planets. That’s because the desolated desert planet of Jakku was the site of much of the early portion of Star Wars Episode VII: The Force Awakens. And this was the planet where Rey grew up and learned how to survive by scavenging for parts.`
         },
         {
             id: 6,
             name: 'Kamino',
-            imageUrl: '../assets/kamino.png',
+            imageUrl: 'assets/kamino.png',
             description: `Of course, one of the most important planets in the entire galaxy was the birthplace of some of the most important warriors in the history of Star Wars. Kamino is a planet that is home to the best cloners in the galaxy and is a place that was well beyond the scope of the Republic before the events of the Clone War.`
         },
         {
             id: 7,
             name: 'Mustafar',
-            imageUrl: '../assets/mustafar.png',
+            imageUrl: 'assets/mustafar.png',
             description: `The Mustafar system doesn’t seem too important at first glance because not a lot of things happened on this planet. However, one of the most pivotal moments in the history of the galaxy happened on Mustafar, especially during the events of Star Wars Episode III: Revenge of the Sith.`
         },
         {
             id: 8,
             name: 'Naboo',
-            imageUrl: '../assets/naboo.png',
+            imageUrl: 'assets/naboo.png',
             description: `The Naboo is so important in the Star Wars continuity because a lot of the most prominent characters came from this planet. For instance, Palpatine was born on this planet, even though he didn’t care much for it. Padme Amidala was also loyal to the Naboo. And, to our dismay, Jar Jar Binks also came from this planet.`
         },
         {
             id: 9,
             name: 'Tatooine',
-            imageUrl: '../assets/tatooine.png',
+            imageUrl: 'assets/tatooine.png',
             description: `Tatooine may not seem like it’s a looker because it’s a desolate desert planet that doesn’t offer much other than sand and gangsters. However, this planet is arguably the most important and most often featured world in the entire Star Wars lore, as Lucasfilm always finds a way to return to this planet.`
         }
     ]
